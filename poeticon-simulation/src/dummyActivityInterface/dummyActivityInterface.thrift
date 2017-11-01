982fc8718ae8ada9d8cd3920bdc7dd385b2020ff
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-11-01 17:34:33 +0000

    poeticon-sim: update contact and copyright

--- poeticon-simulation/src/dummyActivityInterface/dummyActivityInterface.thrift
+++ poeticon-simulation/src/dummyActivityInterface/dummyActivityInterface.thrift
@@ -1,7 +1,7 @@
 # Copyright: (C) 2012-2015 POETICON++, European Commission FP7 project ICT-288382
-# Copyright: (C) 2016 VisLab, Institute for Systems and Robotics,
+# Copyright: (C) 2017 VisLab, Institute for Systems and Robotics,
 #                Instituto Superior Técnico, Universidade de Lisboa, Lisbon, Portugal
-# Author: Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
+# Author: Giovanni Saponaro <gsaponaro@isr.tecnico.ulisboa.pt>
 # CopyPolicy: Released under the terms of the GNU GPL v2.0
 #
 # dummyActivityInterface.thrift
